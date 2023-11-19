83a0e5c51e02f3ebe535c324c85fec026ecb3b60
Amir-Litvak <amirlitvak@gmail.com>
2023-11-19 17:03:46 +0200

    eod

--- telegram_bot_ver/main.py
+++ telegram_bot_ver/main.py
@@ -1,5 +1,5 @@
 import logging
-
+import sys
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes
 
@@ -37,7 +37,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
                                     /start
                                     /help
                                     /download
-                                    /add_item some text""")
+                                    /additem some text/""")
 
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -58,16 +58,21 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 async def download_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     downloader = rss_downloader.RSSDownloader()
     downloader.single_run()
-
+    downloaded_item_list = downloader.get_downloaded_items()
     #print(f"Unpacked list: {*a,}")
-    await update.message.reply_text(f"{downloader.get_downloaded_items()} Added to qBittorrent")
+    if not downloaded_item_list:
+        downloaded_item_list = "No items"
+    await update.message.reply_text(f"{downloaded_item_list} Added to qBittorrent")
 
 async def add_item_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     downloader = rss_downloader.RSSDownloader()
     if not context.args:
-        await update.message.reply_text("Please provide an item name after /add_item")
+        await update.message.reply_text("Please provide an item name after /additem")
     else:
-        downloader.add_item_to_watchlist(tracker='SUBSPLEASE', item=' '.join(context.args))
+        downloader.add_item_to_watchlist(item=' '.join(context.args))
+
+async def exit_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    sys.exit()
 
 
 def main():
@@ -80,7 +85,8 @@ def main():
     application.add_handler(CallbackQueryHandler(button))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("download", download_command))
-    application.add_handler(CommandHandler("add_item",add_item_command))
+    application.add_handler(CommandHandler("additem",add_item_command))
+    application.add_handler(CommandHandler("exit", exit_command))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
--- telegram_bot_ver/rss_downloader.py
+++ telegram_bot_ver/rss_downloader.py
@@ -110,51 +110,21 @@ class RSSDownloader:
         if self._run_flag:
             print("Downloader is alredy running.")
             return
-
-        tracker_list = list()
-
-        with self._lock:
-            tracker_list = list(filter(_only_trackers, self._config.sections()))
-
-        for tracker in tracker_list:
-            self._logger.info(f"Checking List for {tracker}")
-            self._download(tracker)
+        
+        self._download()
            
     def get_downloaded_items(self):
         return self._downloaded_items
 
-    def add_tracker(self, 
-                tracker_name = 'SOMETRACKER', 
-                rss_link_magent = 'https://trackerdomain.com/rss/?r=1080', 
-                rss_link_torr = 'https://trackerdomain.com/rss/?r=1080',
-                download_dir = 'path/to/your/download_dir', 
-                download_method = 'magnet/torr',
-                has_dots = 'no', 
-                must_contain = 'example, 1080p',
-                watch_list = {'placeholder_item' : 'placeholder_path'}):
-        with self._lock:
-            self._config[tracker_name.upper()] = {'rss_link_magnet' : rss_link_magent,
-                                         'rss_link_torr' : rss_link_torr,
-                                    'download_dir' : download_dir,
-                                    'download_method' : download_method,
-                                    'has_dots' : has_dots,
-                                    'must_contain' : must_contain}
-            
-            self._config[f"{tracker_name.upper()}.WATCHLIST"] = watch_list
-            
-            with open(f'{self._curr_dir}/config.ini', 'w+') as configfile:
-                self._config.write(configfile)
-
-            self._logger.info(f"Added {tracker_name} as a new tracker")
     
-    def add_item_to_watchlist(self, tracker, item, path = ''):
+    def add_item_to_watchlist(self, item, path = ''):
         with self._lock:
-            self._config[f"{tracker}.WATCHLIST"][item] = path
+            self._config["WATCHLIST"][item] = path
 
             with open(f'{self._curr_dir}/config.ini', 'w+') as configfile:
                 self._config.write(configfile)
 
-            self._logger.info(f"Added {item} to {tracker} watchlist")
+            self._logger.info(f"Added {item} to watchlist")
 
     def change_setting(self, setting, att):
         with self._lock:
@@ -180,29 +150,27 @@ class RSSDownloader:
         while self._run_flag:
             with self._lock:
                 sleep_time = self._config.getint('SETTINGS' ,'sleep_time')
-                tracker_list = list(filter(_only_trackers, self._config.sections()))
-            for tracker in tracker_list:
-                self._logger.info(f"Checking List for {tracker}")
-                self._download(tracker)
+                self._logger.info("Checking LisT")
+                self._download()
             self._logger.info(f"going to sleep for {sleep_time} seconds")
             time.sleep(sleep_time)
             
-    def _download(self, tracker):
+    def _download(self):
         #returns a list of (name, value) tuples for each entry in 'WATCHLIST'
         with self._lock:
-            rss_link = self._config.get(tracker, 'rss_link_magent') if \
-                            (self._config.get(tracker, 'download_method') == 'magnet') else \
-                            self._config.get(tracker, 'rss_link_torr')
+            rss_link = self._config.get('SETTINGS', 'rss_link_magent') if \
+                            (self._config.get('SETTINGS', 'download_method') == 'magnet') else \
+                            self._config.get('SETTINGS', 'rss_link_torr')
                 
             feed = feedparser.parse(rss_link)
             
-            watch_list = self._config.items(section=f"{tracker}.WATCHLIST")
+            watch_list = self._config.items(section="WATCHLIST")
             for item, dir_path in watch_list:
                 if dir_path == '':
-                    dir_path = f"{self._config.get(tracker, 'download_dir')}{item.title()}/"
+                    dir_path = f"{self._config.get('SETTINGS', 'download_dir')}{item.title()}/"
 
                 # for trackers that name their torrents with dots intsead of spaces
-                if self._config.getboolean(tracker, 'has_dots'):
+                if self._config.getboolean('SETTINGS', 'has_dots'):
                     item = item.replace(" ", ".")
                 
 
@@ -210,14 +178,14 @@ class RSSDownloader:
                     # if item is found in entry titles (lowercased), satisfies additional rules,
                     # and does not exist already in directory.
                     if item in entry.title.lower() and \
-                    self._check_rules(tracker, entry.title) and \
+                    self._check_rules('SETTINGS', entry.title) and \
                     not (os.path.isfile(dir_path + entry.title) or \
                          os.path.isdir(dir_path + entry.title)):
                         self._logger.info(f"Found new entry of {item.title()}")
 
                         # magnet link OR .torrent download
                         if self._config.getboolean('SETTINGS', 'qbit_integration') and \
-                        self._config.get(tracker, 'download_method') == 'magnet':   
+                        self._config.get('SETTINGS', 'download_method') == 'magnet':   
                             self._qb_web(dir_path, entry.link)
                             self._logger.info(f"Added {entry.title} to qBitorrent")
                         elif not os.path.isfile(f"{self._curr_dir}/Downloads/{entry.title}"):
@@ -256,14 +224,3 @@ class RSSDownloader:
         
         qb.download_from_link(link, savepath=(dir_path))    
 
-    
-    async def _telegram_notification(self, msg, token, chat_id):
-        bot = telegram.Bot(token)
-        async with bot:
-            await bot.send_message(text=msg, chat_id=chat_id)
-
-def _only_trackers(section):
-    if (section == 'SETTINGS') or ('.' in section):
-        return False
-
-    return True
